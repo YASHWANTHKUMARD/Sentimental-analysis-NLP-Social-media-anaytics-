faae30581ef0a99a8c0cf6748f3b72124d7bbb6b
YASHWANTHKUMARD <168270104+YASHWANTHKUMARD@users.noreply.github.com>
2026-01-30 21:35:53 +0530

    Add function to clean comments in DataFrame

---
+++ src/clean_comments.py
@@ -0,0 +1,7 @@
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r"[^a-zA-Z\s]", "", text)
+    return text
+
+df["clean_comment"] = df["comment"].apply(clean_text)
